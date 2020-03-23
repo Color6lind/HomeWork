18fcb8446f609f8c0c1ec2608691cf0145637af3
Color6lind <zhigailov42@gmail.com>
2020-03-23 14:33:55 +0700

    remake

--- Regular_Expressions/lesson22/main.js
+++ Regular_Expressions/lesson22/main.js
@@ -4,6 +4,6 @@ console.log("// Task %d //",2);
 
 let str = 'sss site.ru zzz site.com kkk';
 
-let res = str.replace(/[0-9a-z]+\.[0-9a-z]{2,}/g, "http://$&");
+let res = str.replace(/([0-9a-z]+\.[0-9a-z]{2,})/g, '<a href="http://$&">$&</a>"');
 
 console.log(res);
