e6e8c243285d271a372ac09342713604fb8be932
Color6lind <zhigailov42@gmail.com>
2020-04-10 22:52:48 +0700

    tast 3

---
+++ Local_Storage/Task3/main.js
@@ -0,0 +1,48 @@
+
+let btn = document.getElementById('btn');
+let name = document.getElementById('name');
+let em = document.getElementById('em');
+let area = document.getElementById('area');
+
+let form = {
+	name: "",
+	email: "",
+	text: ""
+}
+
+window.addEventListener('onbeforeunload', function() {
+	
+	
+});
+
+name.addEventListener('blur', ()=> {
+	form.name = name.value;
+	localStorage.setItem("ObjectForm", JSON.stringify(form))
+	let raw = localStorage.getItem("ObjectForm")
+	obj = JSON.parse(raw);
+});
+
+em.addEventListener('blur', ()=> {
+	form.email = em.value;
+	localStorage.setItem("ObjectForm", JSON.stringify(form))
+	let raw = localStorage.getItem("ObjectForm")
+	obj = JSON.parse(raw);
+});
+
+area.addEventListener('blur', ()=> {
+	form.text = area.value;
+	localStorage.setItem("ObjectForm", JSON.stringify(form))
+	let raw = localStorage.getItem("ObjectForm")
+	obj = JSON.parse(raw);
+});
+
+window.onload = function() {
+	let last = localStorage.getItem("ObjectForm");
+	obj = JSON.parse(last);
+	console.log(obj)
+	name.value = obj.anme
+	em.value = obj.email
+	area.value = obj.text
+}
+
+// localStorage.clear()
